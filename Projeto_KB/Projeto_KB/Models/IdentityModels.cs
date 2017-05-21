0bdfadc2a9349a326a3a2321b55f820ebe5e17db
Paulo_M_M_Fernandes <paulovirgilio.79@gmail.com>
2017-05-21 11:35:57 +0100

    Change conection String ApplicationContext

--- Projeto_KB/Projeto_KB/Models/IdentityModels.cs
+++ Projeto_KB/Projeto_KB/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Projeto_KB.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("KbaseContext", throwIfV1Schema: false)
         {
         }
 
