1e445713f0c2a8f0b6deaff35eba430317816a76
Paulo_M_M_Fernandes <paulovirgilio.79@gmail.com>
2017-06-04 19:30:16 +0100

    before update DB

---
+++ Projeto_KB/Projeto_KB/Controllers/FaqsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Projeto_KB.DAL;
+using Projeto_KB.Models;
+
+namespace Projeto_KB.Controllers
+{
+    public class FaqsController : Controller
+    {
+        private KbaseContext db = new KbaseContext();
+
+        // GET: Faqs
+        public ActionResult Index()
+        {
+            var faqs = db.Faqs.Include(f => f.Journey);
+            return View(faqs.ToList());
+        }
+
+        // GET: Faqs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faq faq = db.Faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // GET: Faqs/Create
+        public ActionResult Create()
+        {
+            ViewBag.JourneyID = new SelectList(db.Journeys, "ID", "Name");
+            return View();
+        }
+
+        // POST: Faqs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Description,Question,Answer,UrlFaq,JourneyID")] Faq faq)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Faqs.Add(faq);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.JourneyID = new SelectList(db.Journeys, "ID", "Name", faq.JourneyID);
+            return View(faq);
+        }
+
+        // GET: Faqs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faq faq = db.Faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.JourneyID = new SelectList(db.Journeys, "ID", "Name", faq.JourneyID);
+            return View(faq);
+        }
+
+        // POST: Faqs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Description,Question,Answer,UrlFaq,JourneyID")] Faq faq)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(faq).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.JourneyID = new SelectList(db.Journeys, "ID", "Name", faq.JourneyID);
+            return View(faq);
+        }
+
+        // GET: Faqs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faq faq = db.Faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // POST: Faqs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Faq faq = db.Faqs.Find(id);
+            db.Faqs.Remove(faq);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
